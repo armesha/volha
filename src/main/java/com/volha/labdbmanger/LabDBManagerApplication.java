61f62d1518e52d7bb99f572a25de28e9dcb31c7e
Armen Davtian <59493848+armesha@users.noreply.github.com>
2023-11-09 19:15:34 +0100

    .

--- src/main/java/com/volha/labdbmanger/LabDBManagerApplication.java
+++ src/main/java/com/volha/labdbmanger/LabDBManagerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class LabDBManagerApplication {
+public class LabDBManagerApplication {//
 
 	public static void main(String[] args) {
 		SpringApplication.run(LabDBManagerApplication.class, args);
